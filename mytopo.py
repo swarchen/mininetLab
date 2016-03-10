29e3cf29bbdb2a81574dcff7c2bb976ace56631d
jhaocheng <jhaocheng.wu@gmail.com>
2016-03-11 01:32:00 +0800

    topolib tree

--- mytopo.py
+++ mytopo.py
@@ -4,20 +4,15 @@ from mininet.net import Mininet
 from mininet.topo import Topo
 from mininet.util import dumpNodeConnections
 from mininet.log import setLogLevel
+from mininet.link import TCLink
+from mininet.topolib import TreeTopo
 
-class SingleSwitchTopo(Topo):
-    "Single switch connected to n hosts."
-    def build(self, n=2):
-        switch = self.addSwitch('s1')
-        # Python's range(N) generates 0..N-1
-        for h in range(n):
-            host = self.addHost('h%s' % (h + 1))
-            self.addLink(host, switch)
+
 
 def simpleTest():
     "Create and test a simple network"
-    topo = SingleSwitchTopo(n=4)
-    net = Mininet(topo)
+    tree4 = TreeTopo(depth=3,fanout=2)
+    net = Mininet(topo=tree4, link=TCLink)
     net.start()
     print "Dumping host connections"
     dumpNodeConnections(net.hosts)
